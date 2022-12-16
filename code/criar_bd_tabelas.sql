f317a5e45ba7943ee19b3a9bb483bad1efc5bc37
cadim <ricardo.campos@ufjf.edu.br>
2022-12-16 05:15:02 -0300

    Cria tabelas e adiciona documentos
    Cria tabelas e adiciona documentos

--- code/criar_bd_tabelas.sql
+++ code/criar_bd_tabelas.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE IF NOT EXISTS amazonia_tempo;
 
+USE amazonia_tempo;
+
 CREATE TABLE IF NOT EXISTS data (
     id_data INT AUTO_INCREMENT PRIMARY KEY,
     dia int,
@@ -9,20 +11,20 @@ CREATE TABLE IF NOT EXISTS data (
 
 CREATE TABLE IF NOT EXISTS temperatura (
     id_temperatura INT AUTO_INCREMENT PRIMARY KEY,
-    temperatura_tendencia varchar2(255)
+    temperatura_tendencia varchar(255)
 );
 
 CREATE TABLE IF NOT EXISTS localizacao (
     id_localizacao INT AUTO_INCREMENT PRIMARY KEY,
-    cidade varchar2(500),
-    estado varchar2(255),
-    pais varchar2(255)
+    cidade varchar(500),
+    estado varchar(255),
+    pais VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS vento (
     id_vento INT AUTO_INCREMENT PRIMARY KEY,
-    vento_direcao varchar2(50),
-    vento_intensidade varchar2(50)
+    vento_direcao varchar(50),
+    vento_intensidade varchar(50)
 );
 
 CREATE TABLE IF NOT EXISTS tempo (
@@ -32,8 +34,16 @@ CREATE TABLE IF NOT EXISTS tempo (
     media_vento_velocidade_maxima float(10),
     temperatura_minima int,
     temperatura_maxima int,
-    media_temperatura_minima int,
-    media_temperatura_maxima int,
-    condicao_do_tempo_previsca varhchar2(100)
+    media_temperatura_minima FLOAT(10),
+    media_temperatura_maxima FLOAT(10),
+    condicao_do_tempo_previsca varchar(100)
+	id_data INT, 
+	id_localizacao int,
+	id_temperatura int,
+	id_vento int,
+	CONSTRAINT fk_data FOREIGN KEY (id_data) REFERENCES data(id_data),
+	CONSTRAINT fk_localizacao FOREIGN KEY (id_localizacao) REFERENCES localizacao(id_localizacao),
+	CONSTRAINT fk_temperatura FOREIGN KEY (id_temperatura) REFERENCES temperatura(id_temperatura),
+	CONSTRAINT fk_vento FOREIGN KEY (id_vento) REFERENCES vento(id_vento)
 );
 
